f7e78086e1698b662b5e71f9b58624b509004a89
Mateusz <mateuszdobrzanski@outlook.com>
2020-07-03 13:37:53 +0200

    add category model and add reference to product

--- product/models.py
+++ product/models.py
@@ -12,13 +12,33 @@ class Product(models.Model):
         ("damaged", "Damaged product")
     )
     name = models.CharField(max_length=100)
-    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    owner = models.ForeignKey(User,
+                              on_delete=models.CASCADE)
     description = models.TextField(max_length=500)
     condition = models.CharField(max_length=100,
                                  choices=CONDITION_TYPE)
+    category = models.ForeignKey('Category',
+                                 on_delete=models.SET_NULL,
+                                 null=True)
     price = models.DecimalField(max_digits=8,
                                 decimal_places=2)
     created = models.DateTimeField(default=timezone.now)
 
     def __str__(self):
         return self.name
+
+
+# define category model
+class Category(models.Model):
+    category_name = models.CharField(max_length=50)
+    image = models.ImageField(upload_to='product/images',
+                              blank=True,
+                              null=True)
+
+    # display correctly plural form of class name
+    class Meta:
+        verbose_name = 'category'
+        verbose_name_plural = 'categories'
+
+    def __str__(self):
+        return self.category_name
