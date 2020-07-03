95ee280c5373cc8bf6a2ab09f0bcc205260c7855
Mateusz <mateuszdobrzanski@outlook.com>
2020-07-03 12:34:15 +0200

    date and time fill automatically, connect with the user model

--- product/models.py
+++ product/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
 
 
 # define product model
@@ -10,12 +12,13 @@ class Product(models.Model):
         ("damaged", "Damaged product")
     )
     name = models.CharField(max_length=100)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
     description = models.TextField(max_length=500)
     condition = models.CharField(max_length=100,
                                  choices=CONDITION_TYPE)
     price = models.DecimalField(max_digits=8,
                                 decimal_places=2)
-    created = models.DateTimeField()
+    created = models.DateTimeField(default=timezone.now)
 
     def __str__(self):
         return self.name
