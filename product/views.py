44db27ae2435a4f5ad78aba46401df308b5a6914
Mateusz <mateuszdobrzanski@outlook.com>
2020-07-09 10:04:38 +0200

    prepare simple views

--- product/views.py
+++ product/views.py
@@ -1,10 +1,23 @@
 from django.shortcuts import render
+from .models import Product
 
 
 def product_list(request):
-    pass
+    # retrieve all products
+    products = Product.objects.all()
+    # get in console our query list (list of objects from db)
+    # print(products)
+
+    template = 'Product/product_list.html'
+
+    context = {'product_list': products}
+
+    return render(request, template, context)
 
 
 def product_detail(request, id):
-    print(id)
+    product = Product.objects.get(id=id)
+    template = 'Product/product_detail.html'
+    context = {'product_detail': product}
 
+    return render(request, template, context)
