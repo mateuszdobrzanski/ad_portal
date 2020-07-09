e5430cd0aba59d573be15120da18e6c76b3f3e26
Mateusz <mateuszdobrzanski@outlook.com>
2020-07-09 09:24:32 +0200

    add urls redirection, create example views

--- product/urls.py
+++ product/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path(),
+    path('', views.product_list, name='product_list'),
+    path('<int:id>', views.product_detail, name='product_detail'),
 ]
--- product/views.py
+++ product/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def product_list(request):
+    pass
+
+
+def product_detail(request, id):
+    print(id)
+
