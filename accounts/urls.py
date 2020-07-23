3662852747d517a69712ba7aa25d8620400f1497
Mateusz <mateuszdobrzanski@outlook.com>
2020-07-23 23:39:20 +0200

    add url to manage account, add files to use them to create correctly view

--- accounts/urls.py
+++ accounts/urls.py
@@ -8,6 +8,11 @@ urlpatterns = [
 
     # path('login/', LoginView.as_view(template_name='accounts/login.html'), name="login"),
     # changed login method, rename folder 'templates/accounts' to 'templates/registration'
-    path('login/', auth_views.LoginView.as_view()),
+    path('login/', auth_views.LoginView.as_view(), name="login"),
+    path('logout/', auth_views.LogoutView, name="logout"),
+    path('password_change/', auth_views.PasswordChangeView.as_view(), name="password_change"),
+    path('password_change/done/', auth_views.PasswordChangeDoneView.as_view(), name="password_change_done"),
+    path('password_reset/', auth_views.PasswordResetView.as_view(), name="password_reset"),
+    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name="password_reset_done"),
 
 ]
