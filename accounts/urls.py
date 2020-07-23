0ce7a1e61f0b52bf80a30046b5c765d4dbec7c64
Mateusz <mateuszdobrzanski@outlook.com>
2020-07-23 23:14:12 +0200

    changed logon methods

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
-from django.contrib.auth.views import LoginView
+# from django.contrib.auth.views import LoginView
+from django.contrib.auth import views as auth_views
 
 # app_name = 'accounts'
 
 urlpatterns = [
 
-    path('login/', LoginView.as_view(template_name='accounts/login.html'), name="login"),
+    # path('login/', LoginView.as_view(template_name='accounts/login.html'), name="login"),
+    # changed login method, rename folder 'templates/accounts' to 'templates/registration'
+    path('login/', auth_views.LoginView.as_view()),
 
 ]
