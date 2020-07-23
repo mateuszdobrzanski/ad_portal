99f803b702d7b7d8f1a564d9b6de54e11a30e86a
Mateusz <mateuszdobrzanski@outlook.com>
2020-07-23 22:41:34 +0200

    add urls in accounts app, add register "namespace" and register "accounts" in global urls, add simple template for handle login

---
+++ accounts/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from django.contrib.auth.views import LoginView
+
+# app_name = 'accounts'
+
+urlpatterns = [
+
+    path('login/', LoginView.as_view(template_name='accounts/login.html'), name="login"),
+
+]
--- ad_portal/urls.py
+++ ad_portal/urls.py
@@ -24,6 +24,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     # register namespace
     path('products/', include(('product.urls', 'product'), namespace='products')),
+    path('accounts/', include(('accounts.urls', 'accounts'), namespace='accounts')),
     # redirect main page
     path('', views.home),
 ]
